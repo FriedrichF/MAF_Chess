f436685e773031af8fb6b5fdd4b8009468eac5c1
FriedrichF <friedrich.fell87@gmail.com>
2013-04-23 10:03:09 +0200

    Signed-off-by: FriedrichF <friedrich.fell87@gmail.com>

---
+++ src/BoardTest.java
@@ -0,0 +1,38 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class BoardTest {
+
+	@Test
+	public void testBoard() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testBoardInputStream() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testBoardString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testToString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testPrint() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testMove() {
+		fail("Not yet implemented");
+	}
+
+}
